455b3fe48508970f75f9346d74a9aae92d466e9d
rwjmoore <randywilliamjmoore@gmail.com>
2022-04-09 16:59:12 -0600

    update

---
+++ Practice/Week10Practice.m
@@ -0,0 +1,3 @@
+%Problem 2a) 
+G = tf([-1 -7 30], [1 4 3]);
+h = bodeplot(G)
--- Project2/Project2.m
+++ Project2/Project2.m
@@ -1,6 +1,10 @@
+% %tf for the entire plant (including the power dynamics of the amplifier)
+% b = [1200]; %numerator for continuous tf of entire plant
+% a = [1 101.5 150 0]; %denominator for continuous tf of entire plant 
 
-b = [1200]; %numerator for continuous tf
-a = [1 101.5 150 0]; %denominator for continuous tf
+%tf of plant without the power dynamics of the amplifier
+b = [12]; %numerator coefficients
+a = [1 1.5 0];
 Ts = 0.05; % Sample Period in seconds
 Gp_Total = tf(b, a); %continuous time tf
 Gz_Total = c2d(Gp_Total, Ts); %Discrete time tf
@@ -12,11 +16,23 @@ sys = ss(A,B,C,D);
 csys = canon(sys, 'companion'); 
 
 %check for observability
-Ob = obsv(sys); %observability matrix
+Ob = obsv(A,C); %observability matrix
 d = det(Ob);
+%observability is very small, so we can look at the rank of the matrix and
+%compare this with the A matrix to determine if it is observable 
+unob = length(A)-rank(Ob);
+%since unob is 0, this means it is observable. 
+
 %check for controllability
 Co = ctrb(csys); %controllability matrix
 d1 = det(Co); 
+%controllability has passed! 
 
-pole(Gz_Total)
+
+
+
+% pole(Gz_Total);
+% tester = ss(Gz_Total);
+
+
 
