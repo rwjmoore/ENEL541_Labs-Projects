7885bb27bc76f67322c5917e38e088e51dc262ac
andele-fx <90987520+andele-fx@users.noreply.github.com>
2022-04-09 18:06:01 -0600

    Add files via upload

---
+++ Project2/ProjectBlock.m
@@ -0,0 +1,135 @@
+function [sys, x0, str, ts] = ProjectBlock(t,x,u,flag)
+% SFunction for the antenna system
+
+
+switch flag,
+
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0,
+    [sys,x0,str,ts]=mdlInitializeSizes;
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1,
+    sys=mdlDerivatives();
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3,
+    sys=mdlOutputs(x);
+
+  %%%%%%%%%%%%%%%%%%%
+  % Unhandled flags %
+  %%%%%%%%%%%%%%%%%%%
+  case { 2, 4, 9 },
+    sys = [];
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
+% end csfunc
+
+
+%
+%=============================================================================
+% mdlInitializeSizes
+% Return the sizes, initial conditions, and sample times for the S-function.
+%=============================================================================
+%
+
+function [sys,x0,str,ts]=mdlInitializeSizes
+
+
+
+sizes = simsizes;
+sizes.NumContStates  = 2;
+sizes.NumDiscStates  = 0;
+sizes.NumOutputs     = 2;
+sizes.NumInputs      = 1;   
+sizes.DirFeedthrough = 1;
+sizes.NumSampleTimes = 1;
+
+sys = simsizes(sizes);
+x0  = zeros(2,1);
+
+str = [];
+ts  = [0 0];
+
+% end mdlInitializeSizes
+%
+
+%=============================================================================
+% mdlDerivatives
+% Return the derivatives for the continuous states.
+%=============================================================================
+%
+
+function sys=mdlDerivatives()
+
+
+%tf of plant without the power dynamics of the amplifier
+b = [12]; %numerator coefficients
+a = [1 1.5 0];
+Ts = 0.05; % Sample Period in seconds
+Gp_Total = tf(b, a); %continuous time tf
+Gz_Total = c2d(Gp_Total, Ts); %Discrete time tf
+[numz, denz] = tfdata(Gz_Total);
+numz = cell2mat(numz); %convert to appropriate format (array)
+denz = cell2mat(denz); 
+[A,B,C,D] = tf2ss(numz, denz);
+sys = ss(A,B,C,D);
+
+% end mdlDerivatives
+%
+%=============================================================================
+% mdlOutputs
+% Return the block outputs.
+%=============================================================================
+%
+
+
+function sys=mdlOutputs(x)
+
+
+sys =x(1:2);
+
+% end mdlOutputs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
